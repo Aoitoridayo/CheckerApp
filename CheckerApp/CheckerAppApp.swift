a120ed3dc9d2c69484e560a6bf2ed53c6179e7e6
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-21 12:19:41 +0900

    Initial Commit

---
+++ CheckerApp/CheckerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckerAppApp.swift
+//  CheckerApp
+//
+//  Created by 渡邊魁優 on 2023/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
