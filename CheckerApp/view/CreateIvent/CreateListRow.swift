4f40cc2f1ee92b40856172a06f11b8cad7b81a63
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-21 16:59:41 +0900

    commit

---
+++ CheckerApp/view/CreateIvent/CreateListRow.swift
@@ -0,0 +1,18 @@
+//
+//  CreateListRow.swift
+//  CheckerApp
+//
+//  Created by 渡邊魁優 on 2023/11/21.
+//
+
+import SwiftUI
+
+struct CreateListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateListRow()
+}
