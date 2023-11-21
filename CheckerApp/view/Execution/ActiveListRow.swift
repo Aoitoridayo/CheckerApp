7bfbceef1c693b2f21e5bee0dc8d2130204ff01b
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-21 23:31:03 +0900

    commit

---
+++ CheckerApp/view/Execution/ActiveListRow.swift
@@ -0,0 +1,18 @@
+//
+//  ActiveListRow.swift
+//  CheckerApp
+//
+//  Created by 渡邊魁優 on 2023/11/21.
+//
+
+import SwiftUI
+
+struct ActiveListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiveListRow()
+}
